23b586487e9e4f9938c6b17f7abbfbd3763e6917
Grant Johnson <grant@amadensor.com>
2017-02-28 22:21:03 -0700

    Add simple log viewer

---
+++ logs.php
@@ -0,0 +1,18 @@
+<?php
+require_once('db_connect.php');
+require_once('utilities.php');
+
+$check['user']=$_SESSION["user"];
+$check['service']='logs';
+$check['action']='list';
+if (check_perm($check))
+{
+	$query="select userid,execution_dttm,key,request,response,messages from log_messages order by execution_dttm desc limit 100;";
+	$logs=db_retrieve($query);
+	include ('views/show_logs.php');
+}
+else 
+{
+	print"No access";
+}
+?>
---
+++ views/show_logs.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+Show Logs:
+<table border=1>
+<tr><th>User</th><th>Date/Time</th><th>Key</th><th>Messages</th></tr>
+<?php
+foreach ($logs as $log)
+{
+?>
+<tr>
+	<td><?=$log['userid']?></td>
+	<td><?=$log['execution_dttm']?></td>
+	<td><?=$log['key']?></td>
+	<td>
+		<?=$log['request']?><br><br>
+		<?=$log['response']?><br><br>
+	<?php 
+	$messages=json_decode(urldecode($log['messages']));
+	foreach ($messages as $message)
+	{
+		print "$message<br>";
+	}
+	?>
+	</td>
+
+</tr>
+<?php
+}
+?>
+</table>
+</body>
+</html>
